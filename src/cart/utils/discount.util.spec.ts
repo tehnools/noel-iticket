b471bd89222ee0abc55932f47a03c62866d1a49b
tehnools <noelsoongdev@gmail.com>
2023-01-23 07:04:58 +1300

    implement more test cases

---
+++ src/cart/utils/discount.util.spec.ts
@@ -0,0 +1,123 @@
+import { TicketType } from 'src/ticket/dto/ticketType.dto';
+import { discount } from './discount.util';
+
+describe('discount', () => {
+  it(`Given the there are no tickets,
+   it should return 0`, () => {
+    const tickets: TicketType[] = [];
+    const total = discount(tickets);
+    expect(total).toBe(0);
+  });
+
+  it(`Given the amount and number of adults is < 4,
+   it should have no discount`, () => {
+    const tickets: TicketType[] = [
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+    ];
+    const total = discount(tickets);
+    expect(total).toBe(75);
+  });
+
+  it(`Given the amount and number of adults is 4,
+   it should return the right amount with discount of 10%`, () => {
+    const tickets: TicketType[] = [
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+    ];
+    const total = discount(tickets);
+    expect(total).toBe(90);
+  });
+
+  it(`Given the amount and number of adults is 5,
+  it should return the right amount with discount of 10%`, () => {
+    const tickets: TicketType[] = [
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+    ];
+    const total = discount(tickets);
+    expect(total).toEqual(12.5);
+  });
+
+  it(`Given the amount and number of adults is > 2 and children is  2,
+   it should return the right amount is 70`, () => {
+    const tickets: TicketType[] = [
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+    ];
+    const total = discount(tickets);
+    expect(total).toEqual(70);
+  });
+
+  it(`Given the amount and number of adults is > 2 and children is  3,
+  it should return the right amount is 70`, () => {
+    const tickets: TicketType[] = [
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+    ];
+    const total = discount(tickets);
+    expect(total).toEqual(70);
+  });
+
+  it(`Given the amount and number of adults is 4 and children is 4,
+   it should return the right amount is 140`, () => {
+    const tickets: TicketType[] = [
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+    ];
+    const total = discount(tickets);
+    expect(total).toEqual(70);
+  });
+
+  it(`Given the amount and number of adults is 4 and children is 5,
+  it should return the right amount is 140`, () => {
+    const tickets: TicketType[] = [
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+    ];
+    const total = discount(tickets);
+    expect(total).toEqual(70);
+  });
+
+  it(`Given the amount and number of adults is 4 and children is 6,
+   it should return the right amount is 140`, () => {
+    const tickets: TicketType[] = [
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 25, type: 'Adult' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+      { id: 1, price: 15, type: 'Child' },
+    ];
+    const total = discount(tickets);
+    expect(total).toEqual(70);
+  });
+});
--- src/cart/utils/discount.util.ts
+++ src/cart/utils/discount.util.ts
@@ -2,7 +2,39 @@ import { Ticket } from 'src/ticket/dto/ticket.dto';
 import { TicketType } from 'src/ticket/dto/ticketType.dto';
 
 export const discount = (tickets: TicketType[]) => {
+  let discount = 1.0;
+  let numAdults = 0;
+  let numChildren = 0;
   let total = 0;
+  for (const ticket of tickets) {
+    total += ticket.price;
+    if (ticket.type === 'Adult') {
+      numAdults += 1;
+    } else {
+      numChildren += 1;
+    }
+  }
+  const modAdults = numAdults % 2;
+  const modChildrenToAdults = numChildren % numAdults;
+  if (numChildren === 0) {
+    if (numAdults >= 4) {
+      discount = 0.9;
+    }
+  } else {
+    if (modAdults === 0) {
+    }
+  }
+  console.log(
+    'numAdults',
+    numAdults,
+    numChildren,
+    'modAdults',
+    modAdults,
+    'modChildrenToAdults',
+    modChildrenToAdults,
+    'discount',
+    discount,
+  );
 
-  return total;
+  return total * discount;
 };
